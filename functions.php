80fe76e741c8197cff258a9e69586c4af158a620
2Fwebd <help@2f-design.fr>
2017-03-01 14:56:45 +0100

    First release ✌️

---
+++ functions.php
@@ -0,0 +1,91 @@
+<?php
+Class CbyC
+{
+
+    /**
+     * @var string
+     */
+    static $facebook_slug = "kdiamondk";
+
+    /**
+     * CbyC constructor.
+     */
+    public function __construct()
+    {
+        add_action( 'wp_enqueue_scripts', array($this, 'enqueue_styles') );
+    }
+
+    /**
+     * Enqueue Stylesheet
+     */
+    public function enqueue_styles() {
+        wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+    }
+
+    /**
+     * Render Logo
+     */
+    static function renderLogo() {
+
+        ?>
+
+        <div class="site-branding">
+            <a href="<?php site_url(); ?>">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.jpg" alt="<?php _e('Logo CbyC', 'cbyc'); ?>">
+            </a>
+        </div>
+
+        <?php
+
+    }
+
+    /**
+     * Render Facebook script after body tag opening
+     */
+    static function renderFacebookScript() {
+
+        ?>
+
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=759527620890146";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
+
+        <?php
+
+    }
+
+    /**
+     * Render Facebook Module
+     *
+     * @link https://developers.facebook.com/docs/plugins/page-plugin
+     */
+    static function renderFacebook() {
+
+        ?>
+
+        <div class="fb-page"
+             data-href="https://www.facebook.com/<?php echo self::$facebook_slug; ?>/"
+             data-width="300" data-small-header="true"
+             data-adapt-container-width="true"
+             data-hide-cover="false"
+             data-show-facepile="false">
+            <blockquote cite="https://www.facebook.com/<?php echo self::$facebook_slug; ?>/" class="fb-xfbml-parse-ignore">
+                <a href="https://www.facebook.com/<?php echo self::$facebook_slug; ?>/">
+                    <?php echo get_bloginfo('name'); ?>
+                </a>
+            </blockquote>
+        </div>
+
+        <?php
+
+    }
+
+
+}
+new CbyC();
